2b8e9175f6cf442a67fcd26787b6b2253dbd1dbb
sdg9670 <sdg9670@naver.com>
2020-05-16 11:19:46 +0900

    분리

---
+++ lib/pages/login.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(right: 40, left: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'HARU',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Form(
+                child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.grey)),
+                      labelText: 'Email'),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.grey)),
+                      labelText: 'Password'),
+                )
+              ],
+            )),
+            Container(
+                alignment: Alignment.centerRight,
+                child: GestureDetector(
+                  onTap: () {
+                    print('비밀번호 찾기');
+                  },
+                  child: Text(
+                    '비밀번호를 잊으셨나요?',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              color: Colors.blue,
+              child: Text(
+                '로그인',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                print('회원가입');
+              },
+              child: Text(
+                '계정이 없으신가요?',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
