971ae77693ef01fae0b292a1ae7972d3b4fda2e5
sdg9670 <sdg9670@naver.com>
2020-08-23 15:57:54 +0900

    디자인 수정

--- lib/pages/main/home/home.dart
+++ lib/pages/main/home/home.dart
@@ -11,7 +11,6 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          color: Colors.red,
           child: Column(
             children: <Widget>[
               Container(
@@ -24,7 +23,6 @@ class _HomePageState extends State<HomePage> {
               Stack(
                 children: <Widget>[
                   Container(
-                      color: Colors.blue,
                       width: MediaQuery.of(context).size.width,
                       child: Text('asdf')),
                   Align(
