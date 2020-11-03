f44c929d43c55012b33fa10bcd35757956e55c40
sdg9670 <sdg9670@naver.com>
2020-11-03 21:39:27 +0900

    디자인 수정

--- lib/widgets/common/top_bar.dart
+++ lib/widgets/common/top_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:haruapp/services/auth.dart';
 import 'package:haruapp/widgets/common/top_bar_title.dart';
 
 class TopBar extends StatefulWidget implements PreferredSizeWidget {
@@ -19,42 +18,21 @@ class TopBar extends StatefulWidget implements PreferredSizeWidget {
 class _TopBarState extends State<TopBar> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        GestureDetector(
-          onTap: () {
-            AuthService(context: context).updateToken();
-          },
-          child: Container(
-            padding: EdgeInsets.only(right: 1),
-            child: Text(
-              'Haru',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
-        Container(
-          child: Row(
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/main/search');
-                },
-                iconSize: 40,
-              ),
-              IconButton(
-                icon: Icon(Icons.notifications),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/main/notice');
-                },
-                iconSize: 40,
-              )
-            ],
-          ),
-        )
-      ],
-    );
+    return AppBar(title: TopBarTitle(), actions: [
+      IconButton(
+        icon: Icon(Icons.search),
+        onPressed: () {
+          Navigator.pushNamed(context, '/main/search');
+        },
+        iconSize: 40,
+      ),
+      IconButton(
+        icon: Icon(Icons.notifications),
+        onPressed: () {
+          Navigator.pushNamed(context, '/main/notice');
+        },
+        iconSize: 40,
+      )
+    ]);
   }
 }
